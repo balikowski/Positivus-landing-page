5e4e3fc81ebc85423c3a20bf0d0193ff6c378236
balikowski <balikowskid@gmail.com>
2025-12-30 22:16:26 +0100

    added logobar

--- src/components/Logobar/Logobar.jsx
+++ src/components/Logobar/Logobar.jsx
@@ -2,7 +2,12 @@ import styles from './Logobar.module.scss'
 
 function Logobar(){
     return <div className={styles.logobar}>
-        logobar
+        <img src="/amazon.png" alt="amazon logo" className={styles.logo} />
+        <img src="/dribbble.png" alt="dribbble logo" className={styles.logo} />
+        <img src="/hubspot.png" alt="hubspot logo" className={styles.logo} />
+        <img src="/notion.png" alt="notion logo" className={styles.logo} />
+        <img src="/netflix.png" alt="netflix logo" className={styles.logo} />
+        <img src="/zoom.png" alt="zoom logo" className={styles.logo} />
     </div>
 }
 
